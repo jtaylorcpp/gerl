8c58749f764e72d8cec7668be79e3b9725ebd1bd
jtaylorcpp <jtaylorcpp@gmail.com>
2018-09-26 07:40:10 -0400

    working on genserver funcs to make it feel like OTP but still golang

--- genserver.go
+++ genserver.go
@@ -1,25 +1,39 @@
 package gerl
 
-type GenServer interface {
-	Start()
+import (
+	"log"
+)
+
+type GeneralServer interface {
+	Init(GerlState) ProcessID
+	Start() ProcessID
 	CallHandler()
 	CastHandler()
 	Terminate()
 }
 
-type GenServerClient interface {
+type GeneralServerClient interface {
 	Init()
 	Call()
 	Cast()
 	Terminate()
 }
 
-type GServer struct {
+type GenServer struct {
 	Pid   ProcessID
 	State interface{}
 }
 
-func NewGenServer(interface{}) Pid {
+func (gs *GenServer) Init(state GerlState) Pid {
+	log.Println("Initializing GenServer with state: ", state)
+	gs.State = state
+	pid := gs.Start()
+	log.Println("GenServer available at pid: ", pid)
+	return pid
+}
+
+func (gs *GenServer) Start() Pid {
+	//do things to init pid
 	pid := Pid{}
 	return pid
 }
---
+++ state.go
@@ -0,0 +1,3 @@
+package gerl
+
+type GerlState interface{}
